91ff50b8598f0856a5efa96bb8ccd2ebd464727b
Vincent <v3hodgins@gmail.com>
2023-08-07 12:34:08 -0400

    WebSocket Session Implemented

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask_migrate import Migrate
 from models import *
+from flask_socketio import SocketIO, send, emit, join_room
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 app.config['SECRET_KEY'] = 'mysecretkey'  # You should set this to a random string in a real application
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://wackyschemes:B0nerometrics@localhost/rpg_game_db'
 db.init_app(app)
+migrate = Migrate(app, db)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -61,8 +65,104 @@ def register():
     return redirect(url_for('index'))
 
 
+### Game Logic ### 
+
+@app.route('/game', methods=['GET', 'POST'])
+@login_required
+def game():
+    if request.method == 'POST':
+        if 'create_game' in request.form:
+            game_title = request.form['game_title']
+            game_password = request.form['game_password']
+            
+            new_game = Game(title=game_title)
+            new_game.set_password(game_password)
+            
+            db.session.add(new_game)
+            db.session.commit()
+            
+            # Create Gamemaster Player
+            gm_player = Player(user_id=current_user.id, game_id=new_game.id, role="Gamemaster")
+            db.session.add(gm_player)
+            db.session.commit()
+            
+            return redirect(url_for('game_session', game_id=new_game.id))
+        
+        elif 'join_game' in request.form:
+            game_id = request.form['game_id']
+            game_password = request.form['game_password']
+            game = Game.query.get(game_id)
+            
+            if game and game.check_password(game_password):
+                # Check if user is already part of this game
+                existing_player = Player.query.filter_by(user_id=current_user.id, game_id=game.id).first()
+                
+                if not existing_player:
+                    new_player = Player(user_id=current_user.id, game_id=game.id, role="Adventurer")
+                    db.session.add(new_player)
+                    db.session.commit()
+                    
+                    return redirect(url_for('game_session', game_id=game.id))
+                else:
+                    flash('You are already part of this game.', 'warning')
+            else:
+                flash('Invalid game ID or password.', 'danger')
+    
+    # Query games associated with the current user
+    user_games = db.session.query(Game, Player.role).join(Player).filter(Player.user_id == current_user.id).all()
+    
+    return render_template('game.html', user_games=user_games)
+
+
+
+@app.route('/game_session/<int:game_id>', methods=['GET', 'POST'])
+@login_required
+def game_session(game_id):
+    game = Game.query.get(game_id)
+    print(game)
+    if not game:
+        flash('Game not found.', 'danger')
+        return redirect(url_for('game'))
+    
+    # Check if the current user is a part of this game
+    player = Player.query.filter_by(user_id=current_user.id, game_id=game.id).first()
+    if not player:
+        flash('You are not a part of this game.', 'warning')
+        return redirect(url_for('game'))
+
+    if request.method == 'POST':
+        # Handle the game command logic here
+        message_content = request.form.get('command')
+        new_message = GameMessage(game_id=game.id, player_id=player.id, content=message_content)
+        db.session.add(new_message)
+        db.session.commit()
+
+    # Retrieve the last n messages
+    n = 20  # or however many messages you want
+    messages = GameMessage.query.filter_by(game_id=game.id).order_by(GameMessage.timestamp.desc()).limit(n).all()
+
+    return render_template('game_session.html', game=game, player=player, messages=messages)
+
+@socketio.on('send_message')
+def handle_message(data):
+    # Save the message to the database (if you wish to persist it)
+    game_id = data['game_id']
+    message_content = data['content']
+    new_message = GameMessage(game_id=game_id, player_id=current_user.id, content=message_content)
+    db.session.add(new_message)
+    db.session.commit()
+
+    # Emit the new message to all connected clients within the game session
+    room = "game_" + game_id
+    emit('new_message', {'user': data['user'], 'content': data['content']}, room=room)
+
+@socketio.on('join')
+def on_join(data):
+    room = "game_" + data['game_id']
+    join_room(room)
+
 
 ### Run App ###
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app,debug=True)
--- models.py
+++ models.py
@@ -1,20 +1,47 @@
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
 
 db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
-
+    password_hash = db.Column(db.String(128), nullable=False)
+    players = db.relationship('Player', backref='user', lazy=True)
+    
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
-    
+
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    def __repr__(self):
-        return f'<User {self.username}>'
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    players = db.relationship('Player', backref='game', lazy=True)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    game_id = db.Column(db.Integer, db.ForeignKey('game.id'), nullable=False)
+    role = db.Column(db.String(20), nullable=False)  # Either 'Gamemaster' or 'Adventurer'
+    messages = db.relationship('GameMessage', back_populates='player')
+
+
+class GameMessage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    game_id = db.Column(db.Integer, db.ForeignKey('game.id'), nullable=False)
+    player_id = db.Column(db.Integer, db.ForeignKey('player.id'), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    player = db.relationship('Player', back_populates='messages')
